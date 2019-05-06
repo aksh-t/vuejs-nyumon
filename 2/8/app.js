206557d067e2605639bdb503fa9ba4cb0af96607
aksh-t <tanno.ao@gmail.com>
2019-05-06 23:38:14 +0900

    2.8 computed

--- 2/8/app.js
+++ 2/8/app.js
@@ -2,17 +2,17 @@ var items = [
     {
         name: '鉛筆',
         price: 300,
-        quantity: 0
+        quantity: 1
     },
     {
         name: 'ノート',
         price: 400,
-        quantity: 0
+        quantity: 1
     },
     {
         name: '消しゴム',
         price: 500,
-        quantity: 0
+        quantity: 1
     }
 ]
 
@@ -22,11 +22,22 @@ var vm = new Vue({
         items: items
     },
     filters: {
-        numberWithDelimiter: function (value){
+        numberWithDelimiter: function (value) {
             if (!value) {
                 return '0'
             }
             return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+        }
+    },
+    computed: {
+        totalPrice: function () {
+            return this.items.reduce(function (sum, item) {
+                console.log(sum)
+                return sum + (item.price * item.quantity)
+            } ,0)
+        },
+        totalPriceWithTax: function () {
+            return Math.floor(this.totalPrice * 1.08)
         }
     }
 })
